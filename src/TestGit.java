609d450e018790c7fc3b3feaa18962e208417fb7
surya4949 <sprathap049@gmail.com>
2021-05-07 11:15:46 +0530

    This is my first commit comment......

---
+++ src/TestGit.java
@@ -0,0 +1,15 @@
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		String hungry ="yes";
+		if (hungry == "yes")
+			System.out.println("eat something you wish......");
+		else {
+			System.out.println("Do Work......");
+		}
+		
+
+	}
+
+}
